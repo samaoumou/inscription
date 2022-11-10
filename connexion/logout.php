229cac8d54564bd8e0cce8c00e60c4aed6df2a62
issa <i.ndiaye@isepdiamniadio.edu.sn>
2022-11-10 14:23:40 +0000

    termine

--- connexion/logout.php
+++
@@ -1,9 +0,0 @@
-
-        <?php
-            
-            if($_SESSION['email'] !== ""){
-              session_unset();
-              header("location:login.php");
-              session_destroy(); 
-            }
-    ?> 
